21c3c9e2d09726e50f989f9d43b7ed5dd23a7dde
emma <yinhongduan@gmail.com>
2022-06-16 12:04:12 -0700

    finished

--- arrays.js
+++ arrays.js
@@ -2,3 +2,18 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+let backpack = [];
+backpack.push("Lightsaber")
+backpack.push("robe", "food")
+//console.log(backpack)
+let belt = backpack.shift()
+//console.log(belt)
+backpack.splice(1,0,"coat")
+//console.log(backpack.length)
+
+let bag = []
+bag.unshift("blanket", "toothbrush", "flint")
+console.log(bag)
+for (let i = 0; i < 3; i ++){
+    console.log(bag[i])
+}
--- making-decisions-demo/varsAndConditionals.js
+++ making-decisions-demo/varsAndConditionals.js
@@ -3,3 +3,10 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+let kenobiAttack = 20;
+let anikanAttack = 30;
+if (anikanAttack > kenobiAttack){
+    console.log("Anikan has a better attack than kenobi.")
+}
+else 
